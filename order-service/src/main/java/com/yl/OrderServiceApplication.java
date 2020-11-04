b113fe556b727e5fcf1390dd3a48b70997ff1442
ylyang <2111059886@qq.com>
2020-11-04 20:29:32 +0800

    update

--- order-service/src/main/java/com/yl/OrderServiceApplication.java
+++ order-service/src/main/java/com/yl/OrderServiceApplication.java
@@ -1,11 +1,19 @@
 package com.yl;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 /**
  * @author ylyang
  */
+@EnableDiscoveryClient
+@EnableFeignClients
+@MapperScan({"com.yl.repository"})
+//(exclude = DataSourceAutoConfiguration.class)
 @SpringBootApplication
 public class OrderServiceApplication {
     public static void main(String[] args) {
